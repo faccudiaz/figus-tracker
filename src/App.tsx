13377a2ca20c6ac20965725bcabc8ef712394e7a
faccudiaz <faccudiaz@gmail.com>
2022-09-09 20:31:31 -0300

    Fix netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ResponsiveAppBar from './modules/shared/responsiveAppBar/responsiveAppBar';
+// import ResponsiveAppBar from './modules/shared/responsiveAppBar/responsiveAppBar';
 
 function App() {
   return (
