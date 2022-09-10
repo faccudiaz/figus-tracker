b99f789acf4b41e0d8a010063d861b49217f85a4
faccudiaz <faccudiaz@gmail.com>
2022-09-10 20:21:59 -0300

    Clean code stickersPage 2

--- src/modules/stickers/pages/stickersPage/stickersPage.tsx
+++ src/modules/stickers/pages/stickersPage/stickersPage.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import StickersList from '../../components/stickersList/stickersList';
 
 const StickersPage = () => {
