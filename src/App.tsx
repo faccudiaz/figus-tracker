fc34815a3ccb23b476e153cd5b1d3c1d572e6352
faccudiaz <faccudiaz@gmail.com>
2022-09-24 22:52:26 -0300

    Change render

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import ResponsiveAppBar from './modules/shared/responsiveAppBar/responsiveAppBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ResponsiveAppBar from './modules/shared/components/responsiveAppBar/responsiveAppBar';
+import StickersPage from './modules/stickers/pages/stickersPage';
+import LoginPage from './modules/auth/login/pages/loginPage';
+import Register from './modules/auth/register/register';
+import Reset from './modules/auth/reset/reset';
+import Dashboard from './modules/auth/Dashboard/Dashboard';
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="Figus Tracker" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        <div style={{ padding: 25 }}>
+          <Routes>
+            <Route path="/" element={<h1>Pagina de inicio</h1>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="stickers" element={<StickersPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/reset" element={<Reset />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
