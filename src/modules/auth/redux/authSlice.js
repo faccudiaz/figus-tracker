78341c6aa5e9c3c888fe630c1aa0482a01365cd0
faccudiaz <faccudiaz@gmail.com>
2022-09-24 23:05:32 -0300

    add auth slice

--- src/modules/auth/redux/authSlice.js
+++ src/modules/auth/redux/authSlice.js
@@ -3,17 +3,23 @@ import { createSlice } from '@reduxjs/toolkit'
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        user: null
+        user: null,
+        uid: ''
     },
     reducers: {
         setUser: (state, action) => {
             state.user = action.payload
+        },
+        setUid: (state, action) => {
+            state.uid = action.payload
         },
     },
 })
 
 // Action creators are generated for each case reducer function
 export const { setUser } = userSlice.actions
+export const { setUid } = userSlice.actions
+
 export const selectUser = (state) => state.toggle;
 
 
