f8caf06f3b91a073013606ad788d0167cfe48e09
faccudiaz <faccudiaz@gmail.com>
2022-10-04 01:05:13 -0300

    STIKTR-0002 remove imports

--- src/modules/stickers/utils/utilsStickers.ts
+++ src/modules/stickers/utils/utilsStickers.ts
@@ -1,5 +1,4 @@
 import { StickerModel } from '../components/stickersList/stickersListContainer';
-import { addUserSticker, removeUserSticker } from '../services/stickersServices';
 
 export const handleClickSticker = async (userStickers: StickerModel[], sticker: StickerModel) => {
     console.log('handleclickstickerr')
